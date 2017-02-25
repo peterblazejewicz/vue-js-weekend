0a559078239d34e07b54648b8bd744e7a1f1afb8
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2017-02-25 12:03:43 +0100

    List component

---
+++ list-component/index.js
@@ -0,0 +1,27 @@
+var card = new Vue({
+  el: '#card',
+  data: {
+    title: 'Dinosaurs',
+    items: [
+      {
+        text: "Velociraptor"
+      }, {
+        text: "Triceratops"
+      }, {
+        text: "Stegosaurus"
+      }
+    ]
+  },
+  methods: {
+    addItem: function() {
+      var input = document.getElementById('add-dinousaur');
+      if(!!input.value) {
+        this.items.push({text: input.value});
+        input.value = '';
+      }
+    },
+    deleteItem: function(index) {
+      this.items.splice(index, 1);
+    }
+  }
+});
